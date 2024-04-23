497a5fe047ab2906c323143e2ce9e394c048142a
Cainã Mota <caina.mota@hotmail.com>
2024-04-23 00:42:42 -0300

    FEAT: finalizado projeto basico sobre POO utilizando sistema de smartphone como exemplo
    Signed-off-by: Cainã Mota <caina.mota@hotmail.com>

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
+        protected List<string> AplicativosInstalados { get; set; } = new();
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -19,6 +24,12 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+        public void ListarAplicativosInstalados()
+        {
+            System.Console.WriteLine("Aplicativos Instalados:");
+            foreach(var app in AplicativosInstalados)
+                System.Console.WriteLine(app);
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new("9999999", "x1", "123456789", 8);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+nokia.InstalarAplicativo("Instagram");
+nokia.InstalarAplicativo("Outlook");
+nokia.InstalarAplicativo("Banco A");
+nokia.ListarAplicativosInstalados();
+
+
+System.Console.WriteLine("###########################################");
+System.Console.WriteLine("###########################################");
+System.Console.WriteLine("###########################################");
+
+Iphone iphone = new("9999991", "12", "112345678", 12);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("IMessage");
+iphone.InstalarAplicativo("ICloud");
+iphone.ListarAplicativosInstalados();
